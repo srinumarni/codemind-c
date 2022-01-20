c0017d3cb8ba3bdf17c9fa8a0958e806bdfc4c2a
Marni Srinu <97426900+srinumarni@users.noreply.github.com>
2022-01-20 10:52:05 +0530

    #include<stdio.h> int main() {     int n,i,o_sum=0,x[100];     scanf("%d",&n);
        for(i=0;i<n;i++)
         { 
            scanf("%d ",&x[i]);
            if(x[i]%2!=0)
              o_sum+=x[i];
         }
        
          printf("%d",o_sum);
        return 0;
    }

---
+++ ODD Sum.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,o_sum=0,x[100];
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+     { 
+        scanf("%d ",&x[i]);
+        if(x[i]%2!=0)
+          o_sum+=x[i];
+     }
+    
+      printf("%d",o_sum);
+    return 0;
+}
