6bed6a8c09b74221c72ac6f313f19747104a6631
Marni Srinu <97426900+srinumarni@users.noreply.github.com>
2022-02-04 16:03:07 +0530

    #include<stdio.h> int main() {     int x=0,n;     char s[3];     scanf("%d",&n);     while(n>0)     {         scanf("%s",s);         if(s[0]=='+' || s[2]=='+')            x=x+1;         else if(s[0]=='-' || s[2]=='-')            x=x-1;
            n--;
        }
        printf("%d",x);
        return 0;
    }

---
+++ Bits++.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int x=0,n;
+    char s[3];
+    scanf("%d",&n);
+    while(n>0)
+    {
+        scanf("%s",s);
+        if(s[0]=='+' || s[2]=='+')
+           x=x+1;
+        else if(s[0]=='-' || s[2]=='-')
+           x=x-1;
+        
+        n--;
+    }
+    printf("%d",x);
+    return 0;
+}
