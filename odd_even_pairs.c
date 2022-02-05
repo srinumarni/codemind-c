e1082d7551fc1f581d2ef980947c0151daa6b36f
Marni Srinu <97426900+srinumarni@users.noreply.github.com>
2022-02-05 11:39:41 +0530

    #include<stdio.h> int main() {     int n,x[20],e[20],o[20],i,j=0,k=0,min;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&x[i]);         if(x[i]%2==0)           e[j++]=x[i];         else           o[k++]=x[i];     }     min=j>k?k:j;     for(i=0;i<min;i++)     {         printf("%d %d ",o[i],e[i]);     }     if(min==j)        for(i=min;i<k;i++)          printf("%d ",o[i]);     else        for(i=min;i<j;i++)           printf("%d ",e[i]);     if(n%2!=0)        printf("0");     return 0; }

---
+++ odd_even_pairs.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int n,x[20],e[20],o[20],i,j=0,k=0,min;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&x[i]);
+        if(x[i]%2==0)
+          e[j++]=x[i];
+        else
+          o[k++]=x[i];
+    }
+    min=j>k?k:j;
+    for(i=0;i<min;i++)
+    {
+        printf("%d %d ",o[i],e[i]);
+    }
+    if(min==j)
+       for(i=min;i<k;i++)
+         printf("%d ",o[i]);
+    else
+       for(i=min;i<j;i++)
+          printf("%d ",e[i]);
+    if(n%2!=0)
+       printf("0");
+    return 0;
+}
