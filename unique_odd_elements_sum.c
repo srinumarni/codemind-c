ac21b586a50a9f40ec69c57607f9f763c55ece6d
Marni Srinu <97426900+srinumarni@users.noreply.github.com>
2022-02-05 10:54:49 +0530

    #include<stdio.h> int main() {     int n,i,x[50],y[10]={0},sum=0;     scanf("%d",&n);     for(i=0;i<n;i++)      {          scanf("%d",&x[i]);          y[x[i]]++;      }      for(i=1;i<10;i++)      {          if(y[i]!=0 && i%2!=0)             sum=sum+i;      }      printf("%d",sum);      return 0;
    }

---
+++ unique_odd_elements_sum.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,x[50],y[10]={0},sum=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+     {
+         scanf("%d",&x[i]);
+         y[x[i]]++;
+     }
+     for(i=1;i<10;i++)
+     {
+         if(y[i]!=0 && i%2!=0)
+            sum=sum+i;
+     }
+     printf("%d",sum);
+     return 0;
+    
+}
