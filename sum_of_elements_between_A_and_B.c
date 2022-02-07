7b131539b57b46a36b6579f0664076f2ef59c859
Marni Srinu <97426900+srinumarni@users.noreply.github.com>
2022-02-07 14:18:09 +0530

    #include<stdio.h> int main() {     int n,x[40],i,a,b,sum=0;     scanf("%d",&n);     for(i=0;i<n;i++)       scanf("%d",&x[i]);     scanf("%d%d",&a,&b);
        for(i=0;i<n;i++)
        {
            if(x[i]>=a && x[i]<=b)
                sum=sum+x[i];
        }
        
        printf("%d",sum);
        return 0;
    }

---
+++ sum_of_elements_between_A_and_B.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,x[40],i,a,b,sum=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+      scanf("%d",&x[i]);
+    scanf("%d%d",&a,&b);
+    
+    for(i=0;i<n;i++)
+    {
+        if(x[i]>=a && x[i]<=b)
+            sum=sum+x[i];
+    }
+    
+    printf("%d",sum);
+    return 0;
+}
