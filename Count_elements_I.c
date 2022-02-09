bacd94b9cbf6d18d2c36bc95ff64876e23443463
Marni Srinu <97426900+srinumarni@users.noreply.github.com>
2022-02-09 11:00:43 +0530

    #include<stdio.h> int main() {     int x[10],y[10],out1[100]={0},out2[100]={0},i,count=0,m,n;     scanf("%d%d",&m,&n);     for(i=0;i<m;i++)     {         scanf("%d",&x[i]);         out1[x[i]]++;     }     for(i=0;i<n;i++)     {         scanf("%d",&y[i]);         out2[y[i]]++;     }     for(i=0;i<100;i++)     {         if(out1[i]>0 && out2[i]>0)           count++;     }     printf("%d",count);     return 0; }

---
+++ Count_elements_I.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int x[10],y[10],out1[100]={0},out2[100]={0},i,count=0,m,n;
+    scanf("%d%d",&m,&n);
+    for(i=0;i<m;i++)
+    {
+        scanf("%d",&x[i]);
+        out1[x[i]]++;
+    }
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&y[i]);
+        out2[y[i]]++;
+    }
+    for(i=0;i<100;i++)
+    {
+        if(out1[i]>0 && out2[i]>0)
+          count++;
+    }
+    printf("%d",count);
+    return 0;
+}
