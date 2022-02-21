cc99e834cf754146ec7ec59f5e2435bfaad007bc
Marni Srinu <97426900+srinumarni@users.noreply.github.com>
2022-02-21 12:22:07 +0530

    #include<stdio.h> #include<string.h>
    int main()
    {
        char x[20][20],s[50],word[20];
        int i,k=0,j=0;
        scanf("%[^
    ]s",s);
        for(i=0;s[i]!=NULL;i++)
        {
            if(s[i]==' ')
            {
                word[j]=NULL;
                j=0;
               strcpy(x[k++],word);
            }
            else
             {
                 word[j++]=s[i];
             }
        }
        word[j]=NULL;
        strcpy(x[k++],word);
         
        for(i=0;i<k;i++)
        {
            for(j=0;j<k-i-1;j++)
            {
                if(strlen(x[j])>=strlen(x[j+1]))
                {
                    if(strlen(x[j])>strlen(x[j+1]))
                    {
                      char temp[20];
                      strcpy(temp,x[j]);
                      strcpy(x[j],x[j+1]);
                      strcpy(x[j+1],temp);
                    }
                    else
                    {
                        if(strcmp(x[j],x[j+1])==1)
                        {
                           char temp[20];
                           strcpy(temp,x[j]);
                           strcpy(x[j],x[j+1]);
                           strcpy(x[j+1],temp);
                        }
                    }
                   
                 }
                
            }
        }
        for(i=0;i<k;i++)
        printf("%s ",x[i]);
    
        return 0;
    }

---
+++ sort_the_words_based_on_length.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    char x[20][20],s[50],word[20];
+    int i,k=0,j=0;
+    scanf("%[^
+]s",s);
+    for(i=0;s[i]!=NULL;i++)
+    {
+        if(s[i]==' ')
+        {
+            word[j]=NULL;
+            j=0;
+           strcpy(x[k++],word);
+        }
+        else
+         {
+             word[j++]=s[i];
+         }
+    }
+    word[j]=NULL;
+    strcpy(x[k++],word);
+     
+    for(i=0;i<k;i++)
+    {
+        for(j=0;j<k-i-1;j++)
+        {
+            if(strlen(x[j])>=strlen(x[j+1]))
+            {
+                if(strlen(x[j])>strlen(x[j+1]))
+                {
+                  char temp[20];
+                  strcpy(temp,x[j]);
+                  strcpy(x[j],x[j+1]);
+                  strcpy(x[j+1],temp);
+                }
+                else
+                {
+                    if(strcmp(x[j],x[j+1])==1)
+                    {
+                       char temp[20];
+                       strcpy(temp,x[j]);
+                       strcpy(x[j],x[j+1]);
+                       strcpy(x[j+1],temp);
+                    }
+                }
+               
+             }
+            
+        }
+    }
+    for(i=0;i<k;i++)
+    printf("%s ",x[i]);
+
+    return 0;
+}
